194f5f8b25a00e0cbd2b2233ba57f0fa55f75bbb
galyastoyanova <43786376+galyastoyanova@users.noreply.github.com>
2018-12-03 11:33:29 +0200

    Add files via upload

---
+++ Курсова работа-68-Galya Stoyanova/4g.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4g
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, a, z, b;
+            Console.Write("Въведете x:");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Въведете y:");
+            y = double.Parse(Console.ReadLine());
+            Console.Write("Въведете z:");
+            z = double.Parse(Console.ReadLine());
+            Console.Write("Въведете a:");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Въведете b:");
+            b = double.Parse(Console.ReadLine());
+            double result = Math.Pow(y + 6 * a - 2 / 3 * (x + 6), 6) - z / z + 7 * b;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Курсова работа-68-Galya Stoyanova/64.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Length Of A Rectangle:");
+            var a = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Width Of A Rectangle:");
+            var b = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Rectangle Area is");
+            var area = (a * b);
+
+            Console.WriteLine(area);
+            NewMethod();
+            NewMethod1();
+            Console.WriteLine(a * b);
+        }
+
+        private static void NewMethod1()
+        {
+            var b = decimal.Parse(Console.ReadLine());
+        }
+
+        private static void NewMethod()
+        {
+            var a = decimal.Parse(Console.ReadLine());
+        }
+    }
+}
